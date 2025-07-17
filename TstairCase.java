6135869700cd849955af36d6e31f7fcd2b9cb2c3
Mahendra37104 <csefw2317@glbitm.ac.in>
2025-07-17 22:38:29 +0530

    'change

--- TstairCase.java
+++ TstairCase.java
@@ -1,3 +1,18 @@
 public class TstairCase {
-    
+    public static void main(String args[]){
+        int n = 5;
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        for(int i = 1; i<dp.length; i++){
+            if(i == 1){
+                dp[i] = dp[i-1];
+            }
+            else{
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+
+        System.out.println(dp[n]);
+
+    }
 }
